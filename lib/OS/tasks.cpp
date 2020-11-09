30270c81b369e346c618099964c9c9c4030a0387
Carl Baron <carbaro196@gmail.com>
2020-11-09 00:13:58 -0500

    attempt to create cleaner interface, code left commented out cause won't compile

---
+++ lib/OS/tasks.cpp
@@ -0,0 +1,21 @@
+// // #include "FreeRTOS.h"
+// #include "OS/tasks.h"
+
+// namespace picodrone
+// {
+
+// namespace os
+// {
+
+// namespace task
+// {
+
+// xTaskHandle Base::GetHandle() {
+//     return _handle;
+// }
+
+// } // namespace task
+
+// } // namespace os
+
+// } // picodrone namespace
